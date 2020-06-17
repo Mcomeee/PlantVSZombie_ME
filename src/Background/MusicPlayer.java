6f7bbdb424c6da716e771376dd100fdf5a6c39c3
kuki-xie <xieronghaii@outlook.com>
2020-06-17 23:06:17 +0800

    媛姐增加🍒，更改了SunNum,调整了音频

--- src/Background/MusicPlayer.java
+++ src/Background/MusicPlayer.java
@@ -5,6 +5,8 @@ import java.io.File;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
+
 
 public class MusicPlayer {
   private Clip clip; // 全局音乐对象
@@ -18,6 +20,8 @@ public class MusicPlayer {
       audio = AudioSystem.getAudioInputStream(file);
       clip = AudioSystem.getClip();
       clip.open(audio);
+      FloatControl floatControl = (FloatControl)clip.getControl(FloatControl.Type.MASTER_GAIN);
+      floatControl.setValue(-30.0f);
     } catch (Exception e) {
       e.printStackTrace();
     }
